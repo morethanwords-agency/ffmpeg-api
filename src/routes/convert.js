af0ee7698cad90d27276f962b111ecb97a6fd118
Namhyeon, Go <gnh1201@gmail.com>
2024-05-11 02:49:47 +0900

    Update convert.js

--- src/routes/convert.js
+++ src/routes/convert.js
@@ -77,7 +77,8 @@ function convert(req,res,next) {
             '-vf scale=-2:640',
             '-pix_fmt yuv420p',   // for FireFox, Safari
             '-threads 8',
-            '-codec:a libfdk_aac',
+            //'-codec:a libfdk_aac',   // non-free
+            '-codec:a aac',
             '-b:a 128k',
         ];
     }
