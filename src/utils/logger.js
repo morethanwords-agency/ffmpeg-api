ad5413c0041a6b471fb81e7f8cbecb542b957685
MoreThanWords <services@morethanwords.agency>
2025-06-23 16:03:08 +0300

    Update logger.js

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -1,17 +1,17 @@
-//setup custom logger
+// setup custom logger
 const { createLogger, format, transports } = require('winston');
-const { combine, timestamp, label, printf } = format;
+const { combine, timestamp, printf } = format;
 
-const logFormat = printf(({ level, message, label, timestamp }) => {
+const logFormat = printf(({ level, message, timestamp }) => {
   return `${timestamp} ${level.toUpperCase()}: ${message}`;
 });
 
 module.exports = createLogger({
-  format: combine(    
+  format: combine(
     timestamp(),
     logFormat
   ),
   transports: [new transports.Console({
-    level: process.env.LOG_LEVEL || 'info'
+    level: process.env.LOG_LEVEL || 'debug'  // <-- default to debug
   })]
 });
